8c350af3e820c8783d297fe1ff1b70c0826b1de1
Benson Kiai <benson.kiai86@gmail.com>
2023-09-19 21:31:35 +0300

    Update generated sequence

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY (AUTO_INCREMENT) NOT NULL PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL(12,2),
+    weight_kg DECIMAL(12,2)
 );
