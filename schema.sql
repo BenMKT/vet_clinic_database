4b108f6b03a6e184e94ea6593dbc4d3426094c23
Benson Kiai <benson.kiai86@gmail.com>
2023-09-19 20:19:58 +0300

    create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY (AUTO_INCREMENT) NOT NULL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(12,2),
 );
