8477b321c0de0cbe8822e870a50adfc96e77d334
Benson Kiai <benson.kiai86@gmail.com>
2023-09-22 06:14:48 +0300

    Query new added data in mydb database

--- queries.sql
+++ queries.sql
@@ -48,3 +48,48 @@ BEGIN TRANSACTION;
 UPDATE animals SET weight_kg = 20.4 WHERE name = 'Boarmon';
 SELECT * FROM animals;
 COMMIT;
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+\d animals;
+SELECT * FROM owners;
+SELECT* FROM species;
+UPDATE animals SET species_id = CASE WHEN name LIKE '%mon' THEN 2 ELSE 1 END;
+SELECT * FROM animals;
+UPDATE animals
+SET owner_id = (
+  SELECT o.id
+  FROM owners o
+  WHERE o.full_name = CASE
+    WHEN animals.name = 'Agumon' THEN 'Sam Smith'
+    WHEN animals.name IN ('Gabumon', 'Pikachu') THEN 'Jennifer Orwell'
+    WHEN animals.name IN ('Devimon', 'Plantmon') THEN 'Bob'
+    WHEN animals.name IN ('Charmander', 'Squirtle', 'Blossom') THEN 'Melody Pond'
+    WHEN animals.name IN ('Angemon', 'Boarmon') THEN 'Dean Winchester'
+    ELSE NULL
+  END
+);
+SELECT * FROM animals;
+SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT animals.name FROM animals JOIN species ON animals.species_id = species.id WHERE species.id = 1;
+SELECT owners.full_name, animals.name FROM owners LEFT JOIN animals ON owners.id = animals.owner_id;
+SELECT animals.name
+  FROM animals
+  JOIN species ON animals.species_id = species.id
+  JOIN owners ON animals.owner_id = owners.id
+  WHERE species.name = 'digimon' AND owners.full_name = 'Jennifer Orwell';
+SELECT animals.name FROM animals
+  JOIN owners ON animals.owner_id = owners.id
+  WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+SELECT species.name, COUNT(animals.id) AS animal_count FROM species
+  LEFT JOIN animals ON species.id = animals.species_id
+  GROUP BY species.name;
+SELECT o.full_name AS owner, COUNT(a.id) AS animals
+  FROM owners o
+  JOIN animals a ON o.id = a.owner_id
+  GROUP BY o.full_name
+  ORDER BY animals DESC
+  LIMIT 1;
