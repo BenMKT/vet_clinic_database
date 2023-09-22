347f2c1a61db799b66e8a64a25bc99035b2a3f0a
Benson Kiai <benson.kiai86@gmail.com>
2023-09-22 06:15:26 +0300

    Alter Schema

--- schema.sql
+++ schema.sql
@@ -7,5 +7,19 @@ CREATE TABLE animals (
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL(12,2),
-    species VARCHAR(200)
+    species_id INT,
+    owner_id INT,
+    FOREIGN KEY (species_id) REFERENCES species(id),
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
 );
