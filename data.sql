da001d180502e740f9e951eab6214cf3c2d62284
Benson Kiai <benson.kiai86@gmail.com>
2023-09-22 06:16:08 +0300

    Insert data to 2 created tables

--- data.sql
+++ data.sql
@@ -16,3 +16,19 @@ VALUES
 ('Boarmon', '2005-06-07', 7, true, 24.4),
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Ditto', '2022-05-14', 4, true, 22);
+
+--INSERT DATA INTO owners TABLE
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+--INSERT DATA INTO species TABLE
+INSERT INTO species (name)
+VALUES
+('pokemon'),
+('digimon');
